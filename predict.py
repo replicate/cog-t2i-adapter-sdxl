497a60a8d7d9421809855de4959116fb7ea88305
alaradirik <alaradirik@gmail.com>
2023-09-28 23:00:55 +0100

    update repo

--- predict.py
+++ predict.py
@@ -1,45 +1,48 @@
 import os
+from typing import List
+from cog import BasePredictor, Input, Path
 import torch
 import numpy as np
 from PIL import Image
-
-from diffusers import T2IAdapter, AutoencoderKL, StableDiffusionXLAdapterPipeline
-from diffusers import ( 
-    DDIMScheduler,
-    DPMSolverMultistepScheduler,
-    EulerAncestralDiscreteScheduler,
-    EulerDiscreteScheduler,
-    HeunDiscreteScheduler,
-    LMSDiscreteScheduler,
-    PNDMScheduler,
+from diffusers import (
+    T2IAdapter, 
+    AutoencoderKL, 
+    StableDiffusionXLAdapterPipeline, 
+    EulerAncestralDiscreteScheduler
 )
-from huggingface_hub import hf_hub_download, snapshot_download
-from cog import BasePredictor, Input, Path
-from utils import ADAPTER_MAP, ANNOTATOR_MAP, SCHEDULERS
+from utils import SCHEDULERS, install_t2i_adapter_cache
 
 
-MODEL_TYPE = "openpose"
-MODEL_CACHE = "./t2i-adapter-cache"
-MODEL_ADAPTER_CACHE = "./t2i-adapter-cache/adapter"
-MODEL_VAE_CACHE = "./t2i-adapter-cache/sdxl-vae-fp16-fix"
-MODEL_BASE = "stabilityai/stable-diffusion-xl-base-1.0"
+os.environ["HF_HOME"] = os.environ["HUGGINGFACE_HUB_CACHE"] = "/src/hf-cache"
 
+# Available options: "openpose", "lineart", "canny", "sketch", "depth-midas"
+MODEL_TYPE = "depth-midas" 
+
+MODEL_BASE_CACHE = "/src/hf-cache/sdxl-1.0"
+MODEL_ADAPTER_CACHE = f"/src/hf-cache/t2-adapter-{MODEL_TYPE}-sdxl-1.0"
+MODEL_VAE_CACHE = "/src/hf-cache/sdxl-vae-fp16-fix"
+MODEL_SCHEDULER_CACHE = "/src/hf-cache/scheduler"
+MODEL_ANNOTATOR_CACHE = f"/src/hf-cache/annotator/{MODEL_TYPE}"
+
+install_t2i_adapter_cache(
+    model_type=MODEL_TYPE,
+    model_base_cache=MODEL_BASE_CACHE,
+    model_scheduler_cache=MODEL_SCHEDULER_CACHE,
+    model_vae_cache=MODEL_VAE_CACHE,
+    model_adapter_cache=MODEL_ADAPTER_CACHE,
+    model_annotator_cache=MODEL_ANNOTATOR_CACHE,
+)
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
-        if not os.path.exists(MODEL_VAE_CACHE):
-            snapshot_download(repo_id="madebyollin/sdxl-vae-fp16-fix", local_dir=MODEL_VAE_CACHE)
-
-        if not os.path.exists(MODEL_ADAPTER_CACHE):
-            snapshot_download(repo_id=adapter_map[MODEL_TYPE], local_dir=MODEL_ADAPTER_CACHE)
-
-        repo_id, filenames = ANNOTATOR_MAP[MODEL_TYPE]
-        for filename in filenames:
-            hf_hub_download(repo_id=repo_id, filename=filemame, local_dir=MODEL_ANNOTATOR_CACHE)
-
-        # Load data annotator / preprocessor
-        if MODEL_TYPE == "lineart":
+        
+        # Load the annotator
+        if MODEL_TYPE == "openpose":
+            from controlnet_aux.open_pose import OpenposeDetector
+            self.annotator = OpenposeDetector.from_pretrained(MODEL_ANNOTATOR_CACHE).to("cuda")
+        
+        elif MODEL_TYPE == "lineart":
             from controlnet_aux.lineart import LineartDetector
             self.annotator = LineartDetector.from_pretrained(MODEL_ANNOTATOR_CACHE).to("cuda")
 
@@ -51,17 +54,13 @@ class Predictor(BasePredictor):
             from controlnet_aux.pidi import PidiNetDetector
             self.annotator = PidiNetDetector.from_pretrained(MODEL_ANNOTATOR_CACHE).to("cuda")
 
-        elif MODEL_TYPE == "pose":
-            from controlnet_aux import OpenposeDetector
-            self.annotator = OpenposeDetector.from_pretrained(MODEL_ANNOTATOR_CACHE)
-
-        elif MODEL_TYPE == "midas_depth":
+        elif MODEL_TYPE == "depth-midas":
             from controlnet_aux.midas import MidasDetector
             self.annotator = MidasDetector.from_pretrained(
                 MODEL_ANNOTATOR_CACHE, filename="dpt_large_384.pt", model_type="dpt_large"
             ).to("cuda")
 
-        # Load pipeline
+        # Load the pipeline
         vae = AutoencoderKL.from_pretrained(
             MODEL_VAE_CACHE, torch_dtype=torch.float16, local_files_only=True
         )
@@ -70,50 +69,58 @@ class Predictor(BasePredictor):
             MODEL_ADAPTER_CACHE, torch_dtype=torch.float16, varient="fp16", local_files_only=True
         ).to("cuda")
 
+        euler_a = EulerAncestralDiscreteScheduler.from_pretrained(MODEL_SCHEDULER_CACHE)
+
         self.pipe = StableDiffusionXLAdapterPipeline.from_pretrained(
-            MODEL_BASE, 
+            MODEL_BASE_CACHE, 
             vae=vae, 
             adapter=adapter, 
+            scheduler=euler_a,
             torch_dtype=torch.float16, 
-            variant="fp16", 
-            use_safetensors=True,
-            cache_dir=MODEL_CACHE
+            variant="fp16",
         ).to("cuda")
-
+        
         self.pipe.enable_xformers_memory_efficient_attention()
 
     def predict(
         self,
         image: Path = Input(description="Input image"),
-        adapter_name: str = Input(
-            description="Type of adapter to apply to input image",
-            choices=["canny", "sketch", "lineart", "pose", "midas-depth"],
-            default="canny",
-        ),
         prompt: str = Input(
             description="Input prompt",
-            default="A car with flying wings",
+            default="A photo of a room, 4k photo, highly detailed",
         ),
         negative_prompt: str = Input(
             description="Specify things to not see in the output",
-            default="ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs, disfigured, deformed, body out of frame, bad anatomy, watermark, signature, cut off, low contrast, underexposed, overexposed, bad art, beginner, amateur, distorted face",
+            default= "anime, cartoon, graphic, text, painting, crayon, graphite, abstract, glitch, deformed, mutated, ugly, disfigured",
         ),
         num_inference_steps: int = Input(
             description="Number of diffusion steps", ge=0, le=100, default=30
         ),
-        adapter_conditioning_scale: float = Input(description="Factor to scale image by", ge=0, le= , default=0.8),,
-        adapter_conditioning_factor: float = Input(description="Factor to scale image by", ge=0, le=1, default=1),
-        guidance_scale: float = Input(default=7.5),
+        adapter_conditioning_scale: float = Input(
+            description="Conditioning scale", ge=0, le=5.0, default=1.0
+        ),
+        adapter_conditioning_factor: float = Input(
+            description="Factor to scale image by", ge=0, le=1.0, default=1.0
+        ),
+        guidance_scale: float = Input(
+            description="Guidance scale to match the prompt", ge=0, le=10.0, default=7.5
+        ),
+        num_samples: int = Input(
+            description="Number of outputs to generate", ge=1, le=4, default=1
+        ),
         scheduler: str = Input(
             description="Which scheduler to use",
             choices=SCHEDULERS.keys(),
-            default="K_EULER_ANCESTRAL"
+            default="K_EULER_ANCESTRAL",
         )
     ) -> List[Path]:
+        
         """Run a single prediction on the model"""
 
         if scheduler != "K_EULER_ANCESTRAL":
-            self.pipe.scheduler = SCHEDULERS["K_EULER_ANCESTRAL"].from(self.pipe.scheduler.config)
+            self.pipe.scheduler = SCHEDULERS["K_EULER_ANCESTRAL"].from_config(self.pipe.scheduler.config)
+
+        image = Image.open(image).convert("RGB")
 
         if MODEL_TYPE == "lineart":
             image = self.annotator(image, detect_resolution=384, image_resolution=1024)
@@ -121,26 +128,28 @@ class Predictor(BasePredictor):
             image = self.annotator(image, detect_resolution=384, image_resolution=1024)
         elif MODEL_TYPE == "sketch":
             image = self.annotator(image, detect_resolution=1024, image_resolution=1024, apply_filter=True)
-        elif MODEL_TYPE == "pose":
+        elif MODEL_TYPE == "openpose":
             image = self.annotator(image, detect_resolution=512, image_resolution=1024)
             image = np.array(image)[:, :, ::-1]           
             image = Image.fromarray(np.uint8(image)) 
-        elif MODEL_TYPE == "midas_depth":
+        elif MODEL_TYPE == "depth-midas":
             image = self.annotator(image, detect_resolution=512, image_resolution=1024)
 
-        outputs = self.pipe(
+        output = self.pipe(
             prompt=prompt,
             negative_prompt=negative_prompt,
             image=image,
             num_inference_steps=num_inference_steps,
             guidance_scale=guidance_scale, 
             adapter_conditioning_scale=adapter_conditioning_scale,
-            adapter_conditioning_factor=adapter_conditioning_factor
+            adapter_conditioning_factor=adapter_conditioning_factor,
+            num_images_per_prompt=num_samples
         )
-        output_paths = []
-        for i, sample in enumerate(outputs.images):
-            output_path = f"/tmp/out-{i}.png"
-            sample.save(output_path)
-            output_paths.append(Path(output_path))
 
-        return output_paths
+        outputs_paths = []
+        for i, output_image in enumerate(output.images):
+            output_path = f"/tmp/out-{i}.png"
+            output_image.save(output_path)
+            outputs_paths.append(Path(output_path))
+
+        return outputs_paths
--- utils.py
+++ utils.py
@@ -7,21 +7,29 @@ from diffusers import (
     LMSDiscreteScheduler,
     PNDMScheduler,
 )
+import os
+import subprocess
+import tarfile
 
-
-ADAPTER_MAP = {
-  "lineart": "TencentARC/t2i-adapter-lineart-sdxl-1.0",
-  "canny": "TencentARC/t2i-adapter-canny-sdxl-1.0",
-  "sketch": "TencentARC/t2i-adapter-sketch-sdxl-1.0",
-  "openpose": "TencentARC/t2i-adapter-openpose-sdxl-1.0",
-  "depth-midas": "TencentARC/t2i-adapter-depth-midas-sdxl-1.0",
+SDXL_URL_MAP= {
+    "sdxl-1.0": "https://weights.replicate.delivery/default/sdxl/sdxl-1.0.tar",
+    "scheduler": "https://weights.replicate.delivery/default/T2I-Adapter-SDXL/scheduler.tar",
+    "vae-fp16-fix": "https://weights.replicate.delivery/default/T2I-Adapter-SDXL/sdxl-vae-fp16-fix.tar",
 }
 
-ANNOTATOR_MAP = {
-  "lineart": ("lllyasviel/Annotators", ["sk_model.pth", "sk_model2.pth"]),
-  "sketch": ("lllyasviel/Annotators", ["table5_pidinet.pth"]),
-  "openpose": ("lllyasviel/Annotators", ["body_pose_model.pth", "hand_pose_model.pth", "facenet.pth"]), 
-  "depth-midas": ("valhalla/t2iadapter-aux-models", ["dpt_large_384.pt"]),
+ADAPTER_URL_MAP = {
+    "openpose": "https://weights.replicate.delivery/default/T2I-Adapter-SDXL/t2i-adapter-openpose-sdxl-1.0.tar",
+    "lineart": "https://weights.replicate.delivery/default/T2I-Adapter-SDXL/t2i-adapter-lineart-sdxl-1.0.tar",
+    "canny": "https://weights.replicate.delivery/default/T2I-Adapter-SDXL/t2i-adapter-canny-sdxl-1.0.tar",
+    "sketch": "https://weights.replicate.delivery/default/T2I-Adapter-SDXL/t2i-adapter-sketch-sdxl-1.0.tar",
+    "depth-midas": "https://weights.replicate.delivery/default/T2I-Adapter-SDXL/t2i-adapter-depth-midas-sdxl-1.0.tar",
+}
+
+ANNOTATOR_URL_MAP = {
+    "openpose": "https://weights.replicate.delivery/default/T2I-Adapter-SDXL/t2i-openpose-annotator.tar",
+    "lineart": "https://weights.replicate.delivery/default/T2I-Adapter-SDXL/t2i-lineart-annotator.tar",
+    "sketch": "https://weights.replicate.delivery/default/T2I-Adapter-SDXL/t2i-sketch-annotator.tar",
+    "depth-midas": "https://weights.replicate.delivery/default/T2I-Adapter-SDXL/t2i-depth-midas-annotator.tar",
 }
 
 class KarrasDPM:
@@ -38,3 +46,45 @@ SCHEDULERS = {
     "PNDM": PNDMScheduler,
     "LMSDiscrete": LMSDiscreteScheduler,
 }
+
+def download_and_extract(url: str, dest: str):
+    try:
+        if os.path.exists("/src/tmp.tar"):
+            subprocess.check_call(["rm", "/src/tmp.tar"])
+        subprocess.check_call(["pget", url, "/src/tmp.tar"])
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+        raise e
+    tar = tarfile.open("/src/tmp.tar")
+    tar.extractall(dest)
+    tar.close()
+    os.remove("/src/tmp.tar")
+
+def install_t2i_adapter_cache(
+        model_type:str,
+        model_base_cache:str,
+        model_scheduler_cache:str,
+        model_vae_cache:str,
+        model_adapter_cache:str,
+        model_annotator_cache:str
+):
+    # Base Model
+    if not os.path.exists(model_base_cache):
+        os.makedirs(model_base_cache)
+        download_and_extract(SDXL_URL_MAP["sdxl-1.0"], model_base_cache)
+    # Scheduler
+    if not os.path.exists(model_scheduler_cache):
+        os.makedirs(model_scheduler_cache)
+        download_and_extract(SDXL_URL_MAP["scheduler"], model_scheduler_cache)
+    # VAE
+    if not os.path.exists(model_vae_cache):
+        os.makedirs(model_vae_cache)
+        download_and_extract(SDXL_URL_MAP["vae-fp16-fix"], model_vae_cache)
+    # Adapter
+    if not os.path.exists(model_adapter_cache):
+        os.makedirs(model_adapter_cache)
+        download_and_extract(ADAPTER_URL_MAP[model_type], model_adapter_cache)
+    # Annotator
+    if not os.path.exists(model_annotator_cache) and model_type != "canny":
+        os.makedirs(model_annotator_cache)
+        download_and_extract(ANNOTATOR_URL_MAP[model_type], model_annotator_cache)
